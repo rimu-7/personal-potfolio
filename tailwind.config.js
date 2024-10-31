4cbacb3033a682f3b4cb2c19e5da1cd973f45641
rimu-7 <rrimu99@gmail.com>
2024-10-31 21:38:39 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths according to your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
