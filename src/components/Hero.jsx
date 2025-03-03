3a996f32cc191c6cb086d6dc193a4c578fe684fe
rimu-7 <rrimu99@gmail.com>
2025-03-03 16:09:06 +0600

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,57 +14,57 @@ const container = (delay) => ({
 })
 
 const Hero = () => {
-  return  (
-   <div className=" px-4 sm:px-20 ">
-     <div className=" lg:mb-35  border-b border-neutral-900">
-      <div className="flex flex-wrap mb-10 ">
-        <div className="w-full lg:w-1/2">
-          <div className="flex  flex-col items-center lg:items-start">
-            <motion.h1
-              variants={container(0)}
-              initial="hidden"
-              animate="visible"
+  return (
+    <div className=" px-4 sm:px-20 ">
+      <div className=" lg:mb-35  border-b border-neutral-900">
+        <div className="flex flex-wrap mb-10 ">
+          <div className="w-full lg:w-1/2">
+            <div className="flex  flex-col items-center lg:items-start">
+              <motion.h1
+                variants={container(1)}
+                initial="hidden"
+                animate="visible"
 
-              className="pb-4 text-4xl font font-bold  lg:mt-3 lg:text-5xl">
-              Rimu Mutasim Fuad
-            </motion.h1>
-            <motion.span
-              variants={container(0.5)}
-              initial="hidden"
-              animate="visible"
-              className="bg-clip-text text-2xl">
-              Staying Ahead of the Curve
-            </motion.span>
-            <motion.p
-              variants={container(1)}
-              initial="hidden"
-              animate="visible"
-              className="my-8 max-w-3xl py-10 text-2xl  leading-snug text-justify"
-            >
-              {HERO_CONTENT}
-            </motion.p>
+                className="pb-4 text-4xl font font-bold  lg:mt-3 lg:text-5xl">
+                Rimu Mutasim Fuad
+              </motion.h1>
+              <motion.span
+                variants={container(1.5)}
+                initial="hidden"
+                animate="visible"
+                className="bg-clip-text text-2xl">
+                Staying Ahead of the Curve
+              </motion.span>
+              <motion.p
+                variants={container(2)}
+                initial="hidden"
+                animate="visible"
+                className=" max-w-3xl py-10 text-2xl  leading-snug text-justify"
+              >
+                {HERO_CONTENT}
+              </motion.p>
 
 
 
+            </div>
           </div>
-        </div>
-        <div className="w-full lg:w-1/2 lg:p-16">
-          <div className="flex justify-center">
-            <motion.img
-              initial={{ x: 200, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 1, delay: 1.2 }}
+          <div className="w-full lg:w-1/2 lg:p-16">
+            <div className="flex justify-center">
+              <motion.img
+                initial={{ x: 200, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1.5, delay: 1.5 }}
 
-              src={profilePic}
-              alt="Profile"
-              className=" rounded-3xl"
-            />
+                src={profilePic}
+                alt="Profile"
+                className=" rounded-3xl"
+              />
+            </div>
           </div>
         </div>
       </div>
+      <Technologies />
     </div>
-    <Technologies/>
-   </div>
   );
 };
 
